bfe3dafdd25a74c5247c5f62c18c01022f2d512f
JoneyKroney <johnlowriefirst@gmail.com>
2023-09-28 17:05:33 -0500

    added working dir

---
+++ SpringWebpack/SpringWebpack/src/test/java/com/SpringWebpack/SpringWebpackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringWebpack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebpackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
